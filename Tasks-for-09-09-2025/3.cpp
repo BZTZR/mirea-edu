c73c62521086caa4f164b112b00662ce0f0cb0a0
BZTZR <BTR-2007@YANDEX.RU>
2025-09-09 17:38:06 +0300

    Update 3.cpp

--- Tasks-for-09-09-2025/3.cpp
+++ Tasks-for-09-09-2025/3.cpp
@@ -1,9 +1,15 @@
+/*
+Задача 3. Гипотенуза
+Даны два числа a и b.
+Найдите гипотенузу прямоугольного треугольника с катетами a и b.
+*/
 #include <iostream>
 #include <cmath>
 int main()
 {
     int a,b;
-    std::cout << "The hypotenuse finder \n" << "Enter 2 numbers through namespace. For example: 2 2 \n";
+    std::cout << "The hypotenuse finder \n" << "Enter 2 cathets separated by a space in one line. For example: 2 2 \n";
     std::cin>>a>>b;
     std::cout << sqrt(pow(a,2)+pow(b,2))<<" - The hypotenuse of the triangle";
 }
+
