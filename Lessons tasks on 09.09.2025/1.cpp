315b46a430eac02fbef481bcee0b4db0fa617a12
BZTZR <BTR-2007@YANDEX.RU>
2025-09-09 15:34:19 +0300

    Delete Lessons tasks on 09.09.2025 directory

--- Lessons tasks on 09.09.2025/1.cpp
+++
@@ -1,8 +0,0 @@
-#include <iostream>
-int main()
-{
-    int a,b;
-    std::cout<< "Enter 2 number through namespace. For example: 2 2 \n";
-    std::cin>>a>>b;
-    std::cout<< a+b<<" - the sum of entered numbers";
-}
