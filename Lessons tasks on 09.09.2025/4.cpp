e70778b9e91acdef259028733b7d04729adc1015
BZTZR <BTR-2007@YANDEX.RU>
2025-09-09 15:27:21 +0300

    Files uploaded into folder

---
+++ Lessons tasks on 09.09.2025/4.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <string>
+int main()
+{
+    int v,t;
+    std::cout << "Enter 2 numbers through namespace. For example: 2 2 \n";
+    std::cin>>v>>t;
+    int s = v*t;
+    if (s > 109)
+    {
+        std::cout <<"Vasya drove the entire MKAD and drove beyond it "<< (v*t)%109<< " killometers"; 
+    }
+    else
+    {
+        std::cout << (v*t)<<" - km's Vasya drove and "<< 109 - (v*t) << " killometers of road are left";
+    }
+}
