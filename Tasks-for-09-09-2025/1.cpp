a6027a7ec479d762224d1397c50569ba7a3404d2
BZTZR <BTR-2007@YANDEX.RU>
2025-09-09 17:40:03 +0300

    Update 1.cpp

--- Tasks-for-09-09-2025/1.cpp
+++ Tasks-for-09-09-2025/1.cpp
@@ -1,8 +1,14 @@
+/*
+Задача 1. Сумма двух чисел
+Даны два числа A и B (не превосходящие по модулю 2 147 483 647), которые вводятся через пробел.
+Напишите программу, которая выводит их сумму.
+*/
 #include <iostream>
 int main()
 {
     int a,b;
-    std::cout<< "Enter 2 number through namespace. For example: 2 2 \n";
+    std::cout<< "Enter 2 number separated by a space in one line. For example: 2 2 \n";
     std::cin>>a>>b;
     std::cout<< a+b<<" - the sum of entered numbers";
 }
+
