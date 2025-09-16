67d5f307f7834ab6b94da3865ef147190d2431db
BZTZR <BTR-2007@YANDEX.RU>
2025-09-16 18:21:22 +0300

    Update ArithmeticOperations.cpp

--- Tasks_16-09-2025/ArithmeticOperations.cpp
+++ Tasks_16-09-2025/ArithmeticOperations.cpp
@@ -4,7 +4,7 @@ using namespace std;
 int main() {
     long long A, B;
     char op;
-    
+    cout << "Enter 2 numbers and arithmetic operation symbol through space\n. Follow the instructions. For example input: 4 + 5 or 51 * 12\n";
     cin >> A >> op >> B;
     
     switch(op) {
