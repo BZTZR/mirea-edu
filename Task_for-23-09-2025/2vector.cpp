6210ae60661024abab5ec475385f7a5a1aafd60f
BZTZR <BTR-2007@YANDEX.RU>
2025-09-25 10:12:16 +0300

    Update 2vector.cpp

--- Task_for-23-09-2025/2vector.cpp
+++ Task_for-23-09-2025/2vector.cpp
@@ -6,24 +6,18 @@ using namespace std;
 int main() {
     int n;
     cin >> n;
-    
-    // Создаем матрицу n x n
     vector<vector<int>> matrix(n, vector<int>(n));
-    
-    // Заполняем матрицу по правилам
     for (int i = 0; i < n; i++) {
         for (int j = 0; j < n; j++) {
             if (i + j < n - 1) {
-                matrix[i][j] = 0;  // Выше побочной диагонали
+                matrix[i][j] = 0;
             } else if (i + j == n - 1) {
-                matrix[i][j] = 1;  // На побочной диагонали
+                matrix[i][j] = 1;  
             } else {
-                matrix[i][j] = 2;  // Ниже побочной диагонали
+                matrix[i][j] = 2; 
             }
         }
     }
-    
-    // Выводим матрицу
     for (int i = 0; i < n; i++) {
         for (int j = 0; j < n; j++) {
             cout << matrix[i][j];
