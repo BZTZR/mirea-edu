85d1ecfed6c0fdd5bc92f382b9666890568e5298
BZTZR <BTR-2007@YANDEX.RU>
2025-10-01 09:32:42 +0300

    Create notebook-2-1-task.cpp

---
+++ Task-for-data-analytics/notebook-2-1-task.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+#include <string>
+using namespace std;
+int gcd(int first, int second){
+    int raz = 0;
+    bool flag = false;
+    while(flag==false){
+        if (first == second){
+            flag = true;
+        }
+        else if (first<second){
+            raz = second-first;
+            second = first;
+            first = raz;
+        }
+        else{
+            raz = first - second;
+            first = second;
+            second = raz;
+        }
+    }
+    return first;
+}
+int main()
+{
+    int a,b,c,d;
+    cin>>a>>b>>c>>d;
+    cout<<gcd(a,gcd(b,gcd(c,d)));
+}
