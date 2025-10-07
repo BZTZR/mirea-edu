e324a00d1ee8fabc6c3ac3ea39d05c49dab18088
BZTZR <BTR-2007@YANDEX.RU>
2025-10-07 12:00:31 +0300

    Update 7_1.cpp

--- tasks-for-07-10-2025/7_1.cpp
+++ tasks-for-07-10-2025/7_1.cpp
@@ -9,10 +9,12 @@ struct Friend {
     int score3;
 };
 int main() {
+    cout << "How much friends do you have? Enter only 1 number.";
     int n;
     cin >> n;
     Friend friends[100]; 
     for (int i = 0; i < n; ++i) {
+        cout << "Enter surname,name and 3 scores of your friend. Enter this information as many times as you have entered the number of friends.";
         cin >> friends[i].surname 
                  >> friends[i].name 
                  >> friends[i].score1 
