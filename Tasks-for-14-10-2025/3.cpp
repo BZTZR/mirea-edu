0ece517b7726d039d4fc2995d96e8ec9467e3805
BZTZR <BTR-2007@YANDEX.RU>
2025-10-21 10:00:07 +0300

    Update 3.cpp

--- Tasks-for-14-10-2025/3.cpp
+++ Tasks-for-14-10-2025/3.cpp
@@ -1,6 +1,6 @@
 #include <iostream>
 using namespace std;
-int stepen(int a, int n){
+double stepen(double a, double n){
     if(n == 0){
         return 1;
     }
@@ -9,6 +9,12 @@ int stepen(int a, int n){
     }
 }
 int main() {
-    int a = 5, n = 10;
-    cout<<stepen(a,n)<<endl;
+    cout<<"enter 2 numbers on the same line. Number and degree. Degree should be as a non-negative integer"<<endl;
+    double a, n ;
+    cin >> a >>n;
+    if (n < 0 or int(n) != n){
+         cout<<"Restart the program and enter number n correctly"<<endl;
+        exit(0);
+    }
+    cout<<stepen(a,n)<<" - number 'a' in n degree "<<endl;
 }
