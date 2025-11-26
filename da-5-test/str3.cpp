c75ec810712b5b407450abd38a9954103515a79e
BZTZR <BTR-2007@YANDEX.RU>
2025-11-26 09:57:55 +0300

    Create str3.cpp

---
+++ da-5-test/str3.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+#include <vector>
+#include <string>
+using namespace std;
+
+vector<int> prefix_function(const string &s) {
+    int n = (int)s.size();
+    vector<int> pi(n);
+    pi[0] = 0;
+    for (int i = 1; i < n; i++) {
+        int j = pi[i - 1];
+        while (j > 0 && s[i] != s[j]) j = pi[j - 1];
+        if (s[i] == s[j]) j++;
+        pi[i] = j;
+    }
+    return pi;
+}
+
+int main() {
+    string S, P;
+    getline(cin, S);
+    getline(cin, P);
+
+    string combined = P + "#" + S;
+    vector<int> pi = prefix_function(combined);
+
+    for (int i = (int)P.size() + 1; i < (int)combined.size(); i++) {
+        if (pi[i] == (int)P.size()) {
+            cout << i - 2 * (int)P.size() << " ";
+        }
+    }
+    cout << endl;
+    return 0;
+}
